79d4ddc07fa099b208a85b7d70e4ff4dc8b24962
Hongil An <tocrit@gmail.com>
2018-11-25 18:51:16 +0900

    Change tensorflow to pytorch

---
+++ p3_collab_compet/src_pytorch/OUNoise.py
@@ -0,0 +1,27 @@
+"""
+reference: https://github.com/udacity/deep-reinforcement-learning/blob/master/ddpg-pendulum/ddpg_agent.py
+"""
+
+import numpy as np
+import copy
+import random
+
+class OUNoise:
+    def __init__(self, action_size, seed, mu=0., theta=0.15, sigma=0.2, epsilon=0.3):
+        self.mu = mu * np.ones(action_size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.epsilon = epsilon
+        self.action_size = action_size
+        
+        self.reset()
+
+    def reset(self):
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.epsilon * self.sigma * np.random.randn(self.action_size)
+        self.state = x + dx
+        return self.state
